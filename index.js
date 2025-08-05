5d85acf8dc693e19838f1414db7c6a82d23330a1
Priyam Kumar <priyam3451@Priyams-MacBook-Air.local>
2025-08-05 23:34:43 +0530

    Setting for ping

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(express.json())
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'build')));
+//app.use(express.static(path.join(__dirname, 'build')));
 
 // ➡️ ADD THIS HEALTH CHECK ROUTE HERE ⬅️
 // This route will respond to GET requests at the root URL (e.g., https://aristaya-backend-ecommerce.onrender.com/)
